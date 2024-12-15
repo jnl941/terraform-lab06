b4942b9c496508ed982fcefe2594bd62ff652b67
Javier Navarro Lázaro <jnl941@inlumine.ual.es>
2024-12-15 22:51:39 +0100

    sql script

--- Mixto/install_mysql.sh
+++ Mixto/install_mysql.sh
@@ -4,8 +4,9 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password my_password'
 sudo apt-get update
 sudo apt-get -y install mysql-server
-wget https://gist.githubusercontent.com/ualmtorres/55325478004104fbe828683ea5131e40/raw/d8aa8015997ad1c410225eaff7a2c64462d7839a/sginit.sql -O /home/jnl941/sginit.sql
-sudo mysql -h "localhost" -u "root" "-pmy_password" < "/home/jnl941/sginit.sql"
+git clone https://github.com/jnl941/terraform-lab06.git
+#wget https://gist.githubusercontent.com/ualmtorres/55325478004104fbe828683ea5131e40/raw/d8aa8015997ad1c410225eaff7a2c64462d7839a/sginit.sql -O /home/jnl941/sginit.sql
+sudo mysql -h "localhost" -u "root" "-pmy_password" < "/terraform-lab06/Mixto/sginit.sql"
 
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
